996b1d84b48828894c0d780a181d274aaf5731ea
TanujaKhot <ttkhot2004@gmail.com>
2025-02-14 23:42:02 +0530

    Add files via upload

---
+++ withoutobj.java
@@ -0,0 +1,15 @@
+
+public class withoutobj {
+
+	 static void show()
+	{
+		System.out.println("Without object Creation");
+	}
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		withoutobj.show();
+
+	}
+
+}
