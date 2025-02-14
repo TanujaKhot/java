2e45946cdf52474c9df151f3b470a500eb812de9
TanujaKhot <ttkhot2004@gmail.com>
2025-02-14 23:40:57 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,16 @@
+
+public class a {
+	void show()
+	{
+		System.out.println("show World");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		a obj=new a();
+		obj.show();
+		System.out.println("Hello World");
+	}
+
+}
