48378ea145d3b10ca3cce31a0d1ed51760f8224a
TanujaKhot <ttkhot2004@gmail.com>
2025-03-03 23:46:46 +0530

    Add files via upload
    This program is related to the Implicit  Type Casting ,which is one of type of type Casting.

---
+++ TypeCasting.java
@@ -0,0 +1,12 @@
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			int StudentId = 2424;
+			double MoblieNumber= StudentId;
+			System.out.print(MoblieNumber);
+	}
+
+}
