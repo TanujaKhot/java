656a461ea09fe0aac8eb2c617c068771965b744b
TanujaKhot <ttkhot2004@gmail.com>
2025-03-03 23:51:49 +0530

    Add files via upload
    This program is related to Explicit Type casting.

---
+++ explicittype.java
@@ -0,0 +1,22 @@
+
+public class explicittype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		double 	a= 23.234;
+		int b  = (int)a;
+		System.out.println(b);
+
+		
+		
+		
+			double Price = 233.34;
+			int Type=(int)Price;
+			System.out.print(Type);
+			
+		
+	}
+
+}
