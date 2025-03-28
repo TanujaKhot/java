bf2ffe397c995008063edf1e1761759b84e2ee63
TanujaKhot <ttkhot2004@gmail.com>
2025-03-28 22:00:54 +0530

    Add files via upload
    taking string input from user and printing out.

---
+++ input.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class input {
+
+	public static void main(String[] args) {
+		
+		
+	
+				// TODO Auto-generated method stub
+				Scanner ref =new Scanner(System.in);
+				
+				System.out.println("Enter a string:");
+				String a= ref.next(); 
+		
+				System.out.println("String is:"+ a);
+		// TODO Auto-generated method stub
+				
+	}
+
+}
