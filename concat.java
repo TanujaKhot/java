8f5d941e6241e37593cf982c5acd6d0251844842
TanujaKhot <ttkhot2004@gmail.com>
2025-04-08 19:35:24 +0530

    Add files via upload

---
+++ concat.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+public class concat {
+	
+	
+	
+	public static void main(String [] args) {
+		
+	int value1=20;	
+	int value2=30;	
+	String name= "Tanuja";
+	
+	
+	System.out.println(value1+value2);
+	System.out.println(value1+value2+name);
+	}
+	
+
+}
