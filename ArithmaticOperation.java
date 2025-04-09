316e2c437952000951978113189ba5d18c8dfdca
TanujaKhot <ttkhot2004@gmail.com>
2025-04-09 19:39:14 +0530

    Add files via upload

---
+++ ArithmaticOperation.java
@@ -0,0 +1,17 @@
+
+public class ArithmaticOperation {
+
+	public static void main(String[] args) {
+		
+		int value1=50;
+		double value2=40;
+		System.out.println("Perfrom Arithematic Operation");
+		System.out.println(value1+value2);
+		System.out.println(value1-value2);
+		System.out.println(value1*value2);
+		System.out.println(value1/value2);
+		System.out.println(value1%value2);
+
+	}
+
+}
