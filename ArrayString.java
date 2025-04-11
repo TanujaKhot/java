ad6dd2c8b16825942e26a0f2b569016102abc6dc
TanujaKhot <ttkhot2004@gmail.com>
2025-04-12 00:43:09 +0530

    Add files via upload

---
+++ ArrayString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArrayString {
+
+	public static void main(String[] args) {
+		
+		char arr[]= {'j','j','m','c','o','e'};
+		//  TODO Auto-generated method stub
+		String str =new String(arr);
+		System.out.println(str);
+	}
+
+}
